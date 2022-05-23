3bdfd648a55dbf0561b03e5f96fd317f02e915bc
pooh <cktjdgus45@naver.com>
2022-05-23 20:05:44 +0900

    clean app

--- src/index.js
+++ src/index.js
@@ -1,14 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './components/App.jsx';
-import { ApolloProvider } from '@apollo/client';
-import client from './apollo.js';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <ApolloProvider client={client}>
-    <App />
-  </ApolloProvider>,
-
+  <App />
 );
 
